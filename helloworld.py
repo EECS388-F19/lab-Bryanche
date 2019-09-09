b9126899314315e1629bf8f9d199bab72922bc36
bryanche <bryanche@umich.edu>
2019-09-09 12:43:18 -0400

    more python

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print('bryan')
+temp_1 = random.randint(0,100)
+temp_2 = random.randint(0,100)
+print(temp_1)
+print(temp_2)
+temp_3 = temp_1 + temp_2
+print('Sum =', temp_3)
+print('Average =',temp_3 / 2)
